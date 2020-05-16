[python] Fix bug in computation of double integral without updating matrix exponential.

script/consim_py/simulator.py
@@ -274,15 +274,17 @@ class RobotSimulator:
             
             if(update_expm):
                 int_x = self.expMatHelper.compute_integral_x_T(self.A, self.a, x0, dt, self.max_mat_mult)
+                # store int_x because it may be needed to compute int2_x without updating expm in next iteration
+                self.int_x_prev = int_x
                 self.x_pred = self.expMatHelper.compute_x_T(self.A, self.a, x0, dt, self.max_mat_mult)
 #                print("Update x_pred")
             else:
                 int_x = self.expMatHelper.compute_next_integral()
 #                int_x_from_x_pred = self.expMatHelper.compute_integral_x_T(self.A, self.a, self.x_pred, dt, self.max_mat_mult, store=False)
-                int_x_from_x_real = self.expMatHelper.compute_integral_x_T(self.A, self.a, x0, dt, self.max_mat_mult, store=False)
+#                int_x_from_x_real = self.expMatHelper.compute_integral_x_T(self.A, self.a, x0, dt, self.max_mat_mult, store=False)
 #                print("int_x - int_x_from_x_pred", np.max(np.abs(int_x - int_x_from_x_pred)))
 #                print("int_x - int_x_from_x_real", np.max(np.abs(int_x - int_x_from_x_real)))
-                int_x = int_x_from_x_real #+ 1e-9*np.random.rand(int_x.shape[0])
+#                int_x = int_x_from_x_real #+ 1e-9*np.random.rand(int_x.shape[0])
             D_int_x = self.D @ int_x
             dv_mean = dv_bar + JMinv.T @ D_int_x/dt
             
@@ -291,11 +293,13 @@ class RobotSimulator:
                     int2_x = self.expMatHelper.compute_double_integral_x_T(self.A, self.a, x0, dt, self.max_mat_mult)
                 else:
                     int2_x = self.expMatHelper.compute_next_double_integral()
-                    int2_x_from_x_pred = self.expMatHelper.compute_double_integral_x_T(self.A, self.a, self.x_pred, dt, self.max_mat_mult, store=False)
-                    int2_x_from_x_real = self.expMatHelper.compute_double_integral_x_T(self.A, self.a, x0, dt, self.max_mat_mult, store=False)
-                    print("int2_x - int2_x_from_x_pred", np.max(np.abs(int2_x - int2_x_from_x_pred))) #this should be zero but it's not!
-                    print("int2_x - int2_x_from_x_real", np.max(np.abs(int2_x - int2_x_from_x_real)))
+                    int2_x -= dt * self.int_x_prev
+#                    int2_x_from_x_pred = self.expMatHelper.compute_double_integral_x_T(self.A, self.a, self.x_pred, dt, self.max_mat_mult, store=False)
+#                    int2_x_from_x_real = self.expMatHelper.compute_double_integral_x_T(self.A, self.a, x0, dt, self.max_mat_mult, store=False)
+#                    print("int2_x - int2_x_from_x_pred", np.max(np.abs(int2_x - int2_x_from_x_pred))) #this should be zero but it's not!
+#                    print("int2_x - int2_x_from_x_real", np.max(np.abs(int2_x - int2_x_from_x_real)))
 #                    int2_x = int2_x_from_x_real
+#                    int2_x = int2_x_from_x_pred
                 D_int2_x = self.D @ int2_x
                 v_mean = self.v + 0.5*dt*dv_bar + JMinv.T@D_int2_x/dt
             else:

script/consim_py/utils/exponential_matrix_helper.py
@@ -278,26 +278,54 @@ if __name__ == '__main__':
     print("Eigenvalues of A:", np.sort_complex(eigvals(A)).T)
     print("")
     
+    # update_expm
+#    dt = 1.0
+#    int_x = helper.compute_integral_x_T(A, a, x0, dt)
+#    print("compute x")
+#    x_pred = helper.compute_x_T(A, a, x0, dt)
+#    print("compute double int")
+#    int2_x = helper.compute_double_integral_x_T(A, a, x0, dt)
+#
+#    # predict
+#    print("compute next integral")
+#    int_x = helper.compute_next_integral()
+#    print("compute integral")
+#    int_x_from_x_pred = helper.compute_integral_x_T(A, a, x_pred, dt, store=False)
+#    print("int_x - int_x_from_x_pred", np.max(np.abs(int_x - int_x_from_x_pred)))
+#    int2_x = helper.compute_next_double_integral()
+#    print("compute double integral")
+#    int2_x_from_x_pred = helper.compute_double_integral_x_T(A, a, x_pred, dt, store=False)
+#    print("int2_x - int2_x_from_x_pred", np.max(np.abs(int2_x - int2_x_from_x_pred))) #this should be zero but it's not!
+
+
     T = 1
+    x_pred = helper.compute_x_T(A, a, x0, T)
     int_x_T     = helper.compute_integral_x_T(A, a, x0, T)
     int_x_T_2T  = helper.compute_next_integral()
+    int_x_T_2T  = helper.compute_integral_x_T(A, a, x_pred, T)
     int_x_2T_3T = helper.compute_next_integral()
     int_x_2T    = helper.compute_integral_x_T(A, a, x0, 2*T)
     int_x_3T    = helper.compute_integral_x_T(A, a, x0, 3*T)
     print("int_x_T + int_x_T_2T", int_x_T+int_x_T_2T)
     print("int_x_2T            ", int_x_2T)
+    print_error(int_x_2T, int_x_T+int_x_T_2T)
     print("int_x_2T + int_x_2T_3T", int_x_2T+int_x_2T_3T)
     print("int_x_3T              ", int_x_3T)
-    
-    int2_x_T     = helper.compute_double_integral_x_T(A, a, x0, T)
+    print_error(int_x_3T, int_x_2T+int_x_2T_3T)
+
+    x_pred = helper.compute_x_T(A, a, x0, T)
+    int2_x_T     = helper.compute_double_integral_x_T(A, a, x0, T)    
     int2_x_T_2T  = helper.compute_next_double_integral()
+    int2_x_T_2T  = int_x_T*T + helper.compute_double_integral_x_T(A, a, x_pred, T, store=False)
     int2_x_2T_3T = helper.compute_next_double_integral()
     int2_x_2T    = helper.compute_double_integral_x_T(A, a, x0, 2*T)
     int2_x_3T    = helper.compute_double_integral_x_T(A, a, x0, 3*T)
     print("int2_x_T + int_x_T_2T", int2_x_T+int2_x_T_2T)
     print("int2_x_2T            ", int2_x_2T)
+    print_error(int2_x_2T, int2_x_T+int2_x_T_2T)
     print("int2_x_2T + int_x_2T_3T", int2_x_2T+int2_x_2T_3T)
     print("int2_x_3T              ", int2_x_3T)
+    print_error(int2_x_3T, int2_x_2T+int2_x_2T_3T)
     
 #    MAX_MAT_MULT = 0
 #