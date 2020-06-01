e26b8ebb8d182aa06e1336a0d1734e943bb6fa23
Justin Carpentier <justin.carpentier@inria.fr>
2020-06-01 16:49:08 +0200

    core: fix potential bug related to self-assignment

--- src/contact.cpp
+++ src/contact.cpp
@@ -46,7 +46,7 @@ void ContactPoint::secondOrderContactKinematics(pinocchio::Data &data, Eigen::Ve
 void ContactPoint::resetAnchorPoint(const Eigen::Vector3d &p0, bool slipping){
   x_anchor = p0; 
   v_anchor.setZero();
-  slipping = slipping;
+  this->slipping = slipping;
   predictedX0_ = p0;  
 }
 
