[python] Fix anchor point reset in simulator. Move integration errors computation to common file and compute different integration error norms.

script/consim_py/ral2020/solo_trot_cpp.py
@@ -13,7 +13,8 @@ import conf_solo_trot_cpp as conf
 import pinocchio as pin 
 import pickle
 
-from solo_trot_common import load_ref_traj, Empty, state_diff, dt_ref, play_motion, plot_integration_error_vs_ndt
+from solo_trot_common import load_ref_traj, Empty, state_diff, dt_ref, play_motion, \
+    plot_integration_error_vs_ndt, compute_integration_errors
 
 print("".center(conf.LINE_WIDTH, '#'))
 print(" Test Solo Trot C++ ".center(conf.LINE_WIDTH, '#'))
@@ -244,7 +245,7 @@ if(LOAD_GROUND_TRUTH_FROM_FILE):
     print("\nLoad ground truth from file")
     data = pickle.load( open( "solo_trot_cpp.p", "rb" ) )
     
-    i0, i1 = 262*5, 264*5
+    i0, i1 = 1314, 1315
     refX = refX[i0:i1+1,:]
     refU = refU[i0:i1,:]
     feedBack = feedBack[i0:i1,:,:]
@@ -277,44 +278,8 @@ for simu_params in SIMU_PARAMS:
         data[name] = run_simulation(q0, v0, simu_params, data['ground-truth-euler'])
 
 # COMPUTE INTEGRATION ERRORS:
-print('\n')
-ndt = {}
-err_2norm_avg, err_infnorm_avg, err_infnorm_max, err_traj_2norm, err_traj_infnorm = {}, {}, {}, {}, {}
-for name in sorted(data.keys()):
-    if('ground-truth' in name): continue
-    d = data[name]
-    if(d.use_exp_int==0): data_ground_truth = data['ground-truth-euler']
-    else:                 data_ground_truth = data['ground-truth-exp']
-    
-    err_vec = np.empty((2*robot.nv, d.q.shape[1]))
-    err_per_time_2 = np.empty(d.q.shape[1])
-    err_per_time_inf = np.empty(d.q.shape[1])
-    err_2, err_inf = 0.0, 0.0
-    for i in range(d.q.shape[1]):
-        err_vec[:robot.nv,i] = pin.difference(robot.model, d.q[:,i], data_ground_truth.q[:,i])
-        err_vec[robot.nv:,i] = d.v[:,i] - data_ground_truth.v[:,i]
-        err_per_time_2[i]   = norm(err_vec[:,i])
-        err_per_time_inf[i] = norm(err_vec[:,i], np.inf)
-        err_2   += err_per_time_2[i]
-        err_inf += err_per_time_inf[i]
-    err_2 /= d.q.shape[1]
-    err_inf /= d.q.shape[1]
-#    err = (norm(d.q - data_ground_truth.q) + norm(d.v - data_ground_truth.v)) / d.q.shape[0]
-#    err_per_time = np.array(norm(d.q - data_ground_truth.q, axis=0)) + \
-#                    np.array(norm(d.v - data_ground_truth.v, axis=0))
-    err_peak = np.max(err_per_time_inf)
-    print(name, 'Log error 2-norm: %.2f'%np.log10(err_2))
-    if(d.method_name not in err_2norm_avg):
-        err_2norm_avg[d.method_name] = []
-        err_infnorm_max[d.method_name] = []
-        err_infnorm_avg[d.method_name] = []
-        ndt[d.method_name] = []
-    err_2norm_avg[d.method_name] += [err_2]
-    err_infnorm_avg[d.method_name] += [err_inf]
-    err_infnorm_max[d.method_name] += [err_peak]
-    err_traj_2norm[name] = err_per_time_2
-    err_traj_infnorm[name] = err_per_time_inf
-    ndt[d.method_name] += [d.ndt]
+ndt, err_2norm_avg, err_infnorm_avg, err_infnorm_max, err_traj_2norm, err_traj_infnorm = \
+    compute_integration_errors(data, robot)
 
 # PLOT STUFF
 line_styles = 10*['-o', '--o', '-.o', ':o']

script/consim_py/ral2020/solo_trot_py.py
@@ -14,7 +14,8 @@ import conf_solo_trot_py as conf
 import pinocchio as pin 
 import pickle 
 
-from solo_trot_common import load_ref_traj, play_motion, state_diff, dt_ref, Empty
+from solo_trot_common import load_ref_traj, Empty, state_diff, dt_ref, play_motion, \
+    plot_integration_error_vs_ndt, compute_integration_errors
 
 print("".center(conf.LINE_WIDTH, '#'))
 print(" Test Solo Trot Python ".center(conf.LINE_WIDTH, '#'))
@@ -22,7 +23,7 @@ print("".center(conf.LINE_WIDTH, '#'))
 
 # parameters of the simulation to be tested
 i_min = 5
-i_max = i_min+1
+i_max = 6
 i_ground_truth = i_max+2
 
 GROUND_TRUTH_EXP_SIMU_PARAMS = {
@@ -98,7 +99,7 @@ for i in range(i_min, i_max):
 #    }]
     
 PLOT_FORCES = 0
-PLOT_FORCE_PREDICTIONS = 1
+PLOT_FORCE_PREDICTIONS = 0
 PLOT_SLIPPING = 0
 PLOT_BASE_POS = 0
 PLOT_INTEGRATION_ERRORS = 0
@@ -187,6 +188,13 @@ def run_simulation(q0, v0, simu_params, ground_truth):
     try:
         time_start = time.time()
         for i in range(0, N_SIMULATION):
+            if(RESET_STATE_ON_GROUND_TRUTH and ground_truth):
+                # first reset to ensure active contact points are correctly marked because otherwise the second
+                # time I reset the state the anchor points could be overwritten
+                simu.init(ground_truth.q[:,i], ground_truth.v[:,i], reset_anchor_points=True)
+#                print("Reset anchor points to:\n", ground_truth.p0[:,:,i])
+                simu.init(ground_truth.q[:,i], ground_truth.v[:,i], p0=ground_truth.p0[:,:,i].T.reshape(3*nc))
+                
             xact = np.concatenate([simu.q, simu.v])
             diff = state_diff(robot, xact, refX[i])
             results.u[6:,i] = refU[i] + feedBack[i].dot(diff)                 
@@ -212,7 +220,7 @@ def run_simulation(q0, v0, simu_params, ground_truth):
         print("Real-time factor:", t/(time.time() - time_start))
     except Exception as e:
         print(e)
-#        raise e
+        raise e
 
     if conf.use_viewer:
         play_motion(robot, results.q, dt)
@@ -226,7 +234,7 @@ if(LOAD_GROUND_TRUTH_FROM_FILE):
     print("\nLoad ground truth from file")
     data = pickle.load( open( "solo_trot_cpp.p", "rb" ) )
     
-    i0, i1 = 262*5, 264*5
+    i0, i1 = 1314, 1315
     refX = refX[i0:i1+1,:]
     refU = refU[i0:i1,:]
     feedBack = feedBack[i0:i1,:,:]
@@ -261,71 +269,34 @@ for simu_params in SIMU_PARAMS:
         data[name] = run_simulation(q0, v0, simu_params, data['ground-truth-euler'])
 
 # COMPUTE INTEGRATION ERRORS:
-print('\n')
-ndt = {}
-total_err, err_max, err_traj = {}, {}, {}
-for name in sorted(data.keys()):
-    if('ground-truth' in name): continue
-    d = data[name]
-    if(d.use_exp_int==0): data_ground_truth = data['ground-truth-euler']
-    else:                 data_ground_truth = data['ground-truth-exp']
-    
-    err = (norm(d.q - data_ground_truth.q) + norm(d.v - data_ground_truth.v)) / d.q.shape[0]
-    err_per_time = np.array(norm(d.q - data_ground_truth.q, axis=0)) + \
-                    np.array(norm(d.v - data_ground_truth.v, axis=0))
-    err_peak = np.max(err_per_time)
-    print(name, 'Total error: %.2f'%np.log10(err))
-    if(d.method_name not in total_err):
-        total_err[d.method_name] = []
-        err_max[d.method_name] = []
-        ndt[d.method_name] = []
-    total_err[d.method_name] += [err]
-    err_max[d.method_name] += [err_peak]
-    err_traj[name] = err_per_time
-    ndt[d.method_name] += [d.ndt]
+ndt, err_2norm_avg, err_infnorm_avg, err_infnorm_max, err_traj_2norm, err_traj_infnorm = \
+    compute_integration_errors(data, robot)
 
 # PLOT STUFF
 line_styles = 10*['-o', '--o', '-.o', ':o']
 tt = np.arange(0.0, (N_SIMULATION+1)*dt, dt)[:N_SIMULATION+1]
 
-
 # PLOT INTEGRATION ERRORS
 if(PLOT_INTEGRATION_ERRORS):
-    (ff, ax) = plut.create_empty_figure(1)
-    j = 0
-    for name in sorted(total_err.keys()):
-        err = total_err[name]
-        ax.plot(ndt[name], total_err[name], line_styles[j], alpha=0.7, label=name)
-        j += 1
-    ax.set_xlabel('Number of time steps')
-    ax.set_ylabel('Mean error norm')
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    leg = ax.legend()
-    if(leg): leg.get_frame().set_alpha(0.5)
-    
-    (ff, ax) = plut.create_empty_figure(1)
-    j = 0
-    for name in sorted(total_err.keys()):
-        err = total_err[name]
-        ax.plot(ndt[name], err_max[name], line_styles[j], alpha=0.7, label=name)
-        j += 1
-    ax.set_xlabel('Number of time steps')
-    ax.set_ylabel('Max error norm')
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    leg = ax.legend()
-    if(leg): leg.get_frame().set_alpha(0.5)
+    plot_integration_error_vs_ndt(err_2norm_avg, ndt, 'Mean error 2-norm')
+    plot_integration_error_vs_ndt(err_infnorm_avg, ndt, 'Mean error inf-norm')
+    plot_integration_error_vs_ndt(err_infnorm_max, ndt, 'Max error inf-norm')    
     
 if(PLOT_INTEGRATION_ERROR_TRAJECTORIES):
     (ff, ax) = plut.create_empty_figure(1)
-    j = 0
-    for name in sorted(err_traj.keys()):
-        err = err_traj[name]
-        ax.plot(tt, err_traj[name], line_styles[j], alpha=0.7, label=name)
-        j += 1
+    for (j,name) in enumerate(sorted(err_traj_2norm.keys())):
+        ax.plot(tt, err_traj_2norm[name], line_styles[j], alpha=0.7, label=name)
     ax.set_xlabel('Time [s]')
-    ax.set_ylabel('Error norm')
+    ax.set_ylabel('Error 2-norm')
+    ax.set_yscale('log')
+    leg = ax.legend()
+    if(leg): leg.get_frame().set_alpha(0.5)
+    
+    (ff, ax) = plut.create_empty_figure(1)
+    for (j,name) in enumerate(sorted(err_traj_infnorm.keys())):
+        ax.plot(tt, err_traj_infnorm[name], line_styles[j], alpha=0.7, label=name)
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel('Error inf-norm')
     ax.set_yscale('log')
     leg = ax.legend()
     if(leg): leg.get_frame().set_alpha(0.5)

script/consim_py/simulator.py
@@ -30,7 +30,8 @@ class Contact:
         self.frame_id = model.getFrameId(frame_name)
         self.mu = mu
         self.f = zero(3)
-        self.reset_contact_position()
+        self.reset_anchor_point()
+        self.active = False
         
         self.t1 = np.cross(normal, np.array([1., 0., 0.]))
         self.t1 /= norm(self.t1)
@@ -38,14 +39,15 @@ class Contact:
         self.t2 /= norm(self.t2)
         
 
-    def reset_contact_position(self, p0=None):
+    def reset_anchor_point(self, p0=None):
         # Initial (0-load) position of the spring
         if(p0 is None):
             self.p0 = self.data.oMf[self.frame_id].translation.copy()
         else:
             self.p0 = np.copy(p0)
         self.dp0 = np.zeros(3)
-        self.active = False
+        # do not reset active flag otherwise anchor point is overwritten when check_collision is called
+#        self.active = False
         self.slipping = False
         
     def check_collision(self):
@@ -214,7 +216,7 @@ class RobotSimulator:
             self.gui.setLightingMode('world/floor', 'OFF')
 
     # Re-initialize the simulator
-    def init(self, q0, v0=None, reset_contact_positions=False, p0=None):
+    def init(self, q0, v0=None, reset_anchor_points=False, p0=None):
         self.first_iter = True
         self.q = q0.copy()
         if(v0 is None):
@@ -223,18 +225,16 @@ class RobotSimulator:
             self.v = v0.copy()
         self.dv = zero(self.robot.nv)
 
-        if(reset_contact_positions and p0 is None):
+        if(reset_anchor_points and p0 is None):
             # set the contact anchor points at the current contact positions
             se3.forwardKinematics(self.model, self.data, self.q)
             se3.updateFramePlacements(self.model, self.data)
             for (i,c) in enumerate(self.contacts):
-                c.reset_contact_position()
-                self.p0[3*i:3*i+3, 0] = c.p0
+                c.reset_anchor_point()
         elif(p0 is not None):
             # the user is specifying explicitly the anchor points
             for (i,c) in enumerate(self.contacts):
-                c.reset_contact_position(p0[3*i:3*i+3])
-            self.p0 = np.copy(p0)
+                c.reset_anchor_point(p0[3*i:3*i+3])
 
         self.compute_forces()
 
@@ -313,7 +313,7 @@ class RobotSimulator:
                 self.dp0[ 3*i:3*i+3] = c.dp0
                 i += 1
 #                if(contact_changed):
-#                    print(c.frame_name, 'p', c.p.T, 'v', c.v.T, 'f', c.f.T)
+#                print(i, c.frame_name, 'p', c.p.T, 'p0', c.p0.T, 'f', c.f.T)
 
         return self.f
         